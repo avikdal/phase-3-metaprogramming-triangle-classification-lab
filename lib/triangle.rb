2ea10af1e64fd4cbf56a1ab61ce067a19cd9c048
amanda vikdal <vikdal.amanda@gmail.com>
2023-05-22 11:06:07 -0600

    commit tree 5b4a0bfc4fdcdf91f6af8c0d38b905a4bbefb4f5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_reader :l1, :l2, :l3
+
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    valid_triangle
+    if l1 == l2 && l2 == l3
+      :equilateral
+    elsif l1 == l2 || l2 == l3 || l1 == l3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_positive_numbers?
+    [l1, l2, l3].all? { |num| num > 0 }
+  end
+
+  def triangle_inequality?
+    l1 + l2 > l3 && l1 + l3 > l2 && l2 + l3 > l1
+  end
+
+  def valid_triangle
+    raise TriangleError unless sides_positive_numbers? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
